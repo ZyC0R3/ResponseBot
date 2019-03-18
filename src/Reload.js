43fec4a531c3855c32690dc9ee490d1fdc06da29
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2019-03-18 18:35:21 +0000

    1.0.1
    Version Control

--- src/Reload.js
+++ src/Reload.js
@@ -28,7 +28,7 @@ oldListeners = null;
 module.exports = function(robot) {
   var deleteScriptCache, reloadAllScripts, success, walkSync;
   robot.hear(/(what vaur)/i, function(msg) {
-    return msg.send('The current version of Reload that is loaded is JS V1.0');
+    return msg.send('The current version of Reload that is loaded is JS V1.0.1');
   });
   robot.respond(/reload/i, {
     id: 'reload-scripts.reload'
