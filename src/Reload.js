a6a869aafa2cf1db3c567342dbbc90ffee2765c1
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2019-06-05 23:14:53 +0100

    1.0.2
    Code Relicensing - Project is now under GPL-3.0

--- src/Reload.js
+++ src/Reload.js
@@ -5,12 +5,12 @@
 //  None
 //
 // Configuration:
-//  Install via internal Hubot with CoffeeScript or via npm install responsebot --save
+//  Installed as part of ResponseBot via npm install responsebot --save
 //
 // Commands:
 //  Reload = Reloads script
 //
-// Author:
+// Original Authors:
 //  spajus
 //  vinta
 //  m-seldin
@@ -28,7 +28,7 @@ oldListeners = null;
 module.exports = function(robot) {
   var deleteScriptCache, reloadAllScripts, success, walkSync;
   robot.hear(/(what vaur)/i, function(msg) {
-    return msg.send('The current version of Reload that is loaded is JS V1.0.02');
+    return msg.send('The current version of Reload that is loaded is JS V1.0.2');
   });
   robot.respond(/reload/i, {
     id: 'reload-scripts.reload'
