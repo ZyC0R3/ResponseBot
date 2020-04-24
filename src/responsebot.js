6b690538cedcd4969f450b47325eb8d75b41a00b
Liam Moore <39091558+ZyC0R3@users.noreply.github.com>
2020-04-24 13:35:43 +0100

    7.1.5

--- src/responsebot.js
+++ src/responsebot.js
@@ -45,7 +45,7 @@ module.exports = function(robot) {
   // KB Version Info
   // ---
   robot.hear(/(what vaur)/i, function(msg) {
-    return msg.send('The current version of ResponseBot that is loaded is JS V7.1.2');
+    return msg.send('The current version of ResponseBot that is loaded is JS V7.1.5');
   });
   // ---
   // KB Responses with user Triggers
